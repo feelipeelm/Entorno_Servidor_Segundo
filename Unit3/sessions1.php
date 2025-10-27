4cb26f5a300540ce5e59914e0605ea0c0b045988
Felipe Luna <lunamerlofelipe@gmail.com>
2025-10-27 10:20:11 +0100

    SESSIONS2

--- Unit3/sessions1.php
+++ Unit3/sessions1.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,30 +10,41 @@
 </head>
 <body>
     <?php
-    session_start();
     // Set session variables
     $_SESSION["t-shirt"] = " ";
-
-
+    if(isset($_POST["T-shirts"])) && (isset($_POST["Jumpers"])) && (isset($_POST["Jeans"])){
     ?>
-    
     <form>
-         <label for="lang">T-Shirts</label>
-        <select name="products" id="lang">
+       <label for="lang">T-Shirts</label>
+        <select name="T-shirts" id="lang">
          <option value="red">Long-sleeves, Red</option>
          <option value="green">Short-sleeves, Green</option>
          <option value="pink">Sleevesless, Pink</option>
         </select>
         <br>
+        <input type="submit" value="Submit">
+    </form>
+    <?php
+    }
+    if (isset($_POST["T-shirts"])){ $_SESSION["Shirt"]=$_POST["T-shirts"];
+    ?>
+    <form>
          <label for="lang">Jumpers</label>
-        <select name="products" id="lang">
+        <select name="Jumpers" id="lang">
           <option value="blue">Hoodie, Blue</option>
           <option value="black">Zipper, Black</option>
           <option value="grey">Crewneck, Grey</option>
         </select>
         <br>
+        <input type="submit" value="Submit">
+    </form>
+    <?php
+    }
+    if (isset($_POST["Jumpers"])){ $_SESSION["Jumper"]=$_POST["Jumpers"];
+    ?>
+    <form>
         <label for="lang">Jeans</label>
-        <select name="products" id="lang">
+        <select name="Jeans" id="lang">
           <option value="darkblue">Skinny, Dark Blue</option>
           <option value="lightblue">Regular, Light Blue</option>
           <option value="black">Ripped, Black</option>
@@ -38,5 +52,7 @@
         <br>
         <input type="submit" value="Submit">
     </form>
+    <?php
+    }
 </body>
 </html>
